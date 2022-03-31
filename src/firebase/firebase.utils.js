0499867b6419f1c62da03e34f4f513026b72cc99
Gabor Szabo <gabor.szabo86@hotmail.com>
2022-03-31 10:12:44 +0200

    Update firebase and react-scripts

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,15 +1,15 @@
-import firebase from 'firebase/app';
-import 'firebase/firestore';
-import 'firebase/auth';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
 
 const config = {
-    apiKey: "AIzaSyChbZqM59myoQPf3z4qGBVJELYmRpr-GC0",
-    authDomain: "crwn-db-a9e37.firebaseapp.com",
-    databaseURL: "https://crwn-db-a9e37.firebaseio.com",
-    projectId: "crwn-db-a9e37",
-    storageBucket: "crwn-db-a9e37.appspot.com",
-    messagingSenderId: "444488395436",
-    appId: "1:444488395436:web:49974c526038ad35c18d09"
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    databaseURL: process.env.DATABASE_URL,
+    projectId: process.env.PROJECT_ID,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID,
+    appId: process.env.APP_ID
 };
 
 firebase.initializeApp(config);
